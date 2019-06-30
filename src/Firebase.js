e3c38976634212ab1cb4baab6fb4c9a0bcd417b0
ksokolov <spnqiddqd@riseup.net>
2019-06-30 21:52:32 +0300

    Remove unused imports, add import for increment

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,14 +2,11 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 import firebase from 'firebase/app';
-import { authState } from 'rxfire/auth';
-import { collectionData } from 'rxfire/firestore';
-import { filter } from 'rxjs/operators';
 
-const app = firebase.initializeApp();
+const app = firebase.initializeApp({}); //provide firebase creds 
 
-const firestore = firebase.firestore(app); // Initialize firestore
-const increment =  firebase.firestore.FieldValue.increment
+const firestore = firebase.firestore(app);
+const increment =  firebase.firestore.FieldValue.increment;
 
 export { app, firestore, increment };
 
